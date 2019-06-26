d4c1dc0cecf87f6199e714df6ee58e1a301091c0
Rayanne B. Lima <rayanne_lima2010@hotmail.com>
2019-06-26 13:39:13 -0300

    Update conexao.php

--- phpFiles/conexao.php
+++ phpFiles/conexao.php
@@ -1,13 +1,13 @@
 <?php
 header('Content-type: text/html; charset=UTF-8; application/json');
-
 //Fazendo conexão com o banco.
-$host = "sql112.epizy.com";
-$user="epiz_23848714";
-$pass="rayanne22";
-$banco = "epiz_23848714_bdrolenoifapp";
+$host = "localhost";
+$user="id9517233_rayanne";
+$pass="";
+$banco = "id9517233_bd_rolenoifapp";
 $conexao = mysqli_connect($host,$user,$pass,$banco) or die(mysqli_error());
 mysqli_select_db($conexao,$banco) or die(mysqli_error());
+session_start();
 
 //TODO: verificar se é necessario session_start para manter usuario conectado no app tambem
 ?>
